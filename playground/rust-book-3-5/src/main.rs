dc02599f623f85494d4b27551a1828834af3a40a
Millimeter <akumakumu@gmail.com>
2025-12-29 23:29:50 +0700

    3.5

---
+++ playground/rust-book-3-5/src/main.rs
@@ -0,0 +1,69 @@
+fn main() {
+    let number = 7;
+
+    if number < 5 {
+        println!("true");
+    } else {
+        println!("false");
+    }
+
+    let number = 3;
+
+    if number != 0 {
+        println!("true");
+    }
+
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    let condition = true;
+
+    let number = if condition { 5 } else { 6 };
+
+    println!("number : {number}");
+
+    // loop {
+    //     println!("again!");
+    // }
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("result : {result}");
+
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+}
