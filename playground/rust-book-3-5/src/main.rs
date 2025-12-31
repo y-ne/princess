df0fdc1475e3aea55e4573f87741cbb8973beb1d
Millimeter <akumakumu@gmail.com>
2025-12-31 22:34:13 +0700

    3.5 after

--- playground/rust-book-3-5/src/main.rs
+++ playground/rust-book-3-5/src/main.rs
@@ -66,4 +66,35 @@ fn main() {
         count += 1;
     }
     println!("End count = {count}");
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("value : {element}");
+    }
+
+    for number in (1..4).rev() {
+        println!("{number}");
+    }
+
+    println!("LIFTOFF!!");
 }
